16af346b9deeab694890e1d719f93e0d6a07947d
Benji <benjaminpeto23@gmail.com>
2021-07-23 15:09:09 +0100

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,34 @@
+const Clarifai = require('clarifai');
+const { json } = require('express');
+
+// API of face recognition
+const app = new Clarifai.App({
+  apiKey: process.env.API_KEY,
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => res.status(400).json('Unable to get call from API'))
+}
+
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("Unable to get entries"));
+};
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
+};
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const { response } = require("express");
+require('dotenv').config();
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "benji",
+    password: "fuertesurf23",
+    database: "face-recognition-db",
+  },
+});
+
+const app = express();
+
+// bodyParser, midware (has to be after express was called)
+
+app.use(express.json());
+app.use(cors());
+
+// ROOT
+
+app.get("/", (req, res) => {
+  res.send("success");
+});
+
+// SIGN IN
+
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+// REGISTER
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+// PROFILE
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+// IMAGE
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+// CLARIFAI API
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+// PORT
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port: ${process.env.PORT}`);
+});
+
+/*
+    STRUCTURE
+
+/                --> res = this is working
+/signin          --> POST = success/fail
+/register        --> POST = user 
+/profile/:userId --> GET = user
+/image           --> PUT = user
+
+*/
