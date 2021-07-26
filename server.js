8bc6b8d4dc079c8fc7fe21c35b16ec763608c23e
Benji <benjaminpeto23@gmail.com>
2021-07-26 12:56:16 +0100

    changed database host

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-metric-49997",
     user: "benji",
     password: "fuertesurf23",
     database: "face-recognition-db",
