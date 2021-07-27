49e912489a52d261f5bb117113ee5fc88398306f
Benji <benjaminpeto23@gmail.com>
2021-07-27 20:39:26 +0100

    consol logging errors

--- controllers/register.js
+++ controllers/register.js
@@ -12,6 +12,7 @@ const handleRegister = (req, res, db, bcrypt) => {
         .into("login")
         .returning("email")
         .then(loginEmail => {
+          console.log(loginEmail);
           return trx("users")
             .returning("*")
             .insert({
